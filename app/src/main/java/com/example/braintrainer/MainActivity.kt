f625a0b907b00574e0a28349bba2c0e4c28ffb0a
docbrown85 <me@example.com>
2020-10-27 15:05:43 +0100

    Add starting button

--- app/src/main/java/com/example/braintrainer/MainActivity.kt
+++ app/src/main/java/com/example/braintrainer/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.braintrainer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var goButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goButton = findViewById(R.id.goButton)
+    }
+
+    fun start(view: View) {
+        goButton.visibility = View.INVISIBLE
     }
 }
